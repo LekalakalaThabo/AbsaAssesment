956b712eb205737a3e3e7d7be232540dc87d3b6d
tlekalakala <thabolekalakala@standardbank.co.za>
2020-06-26 13:49:44 +0200

    fixedRole Locators

--- Absa_WebApp_Assesment/src/test/java/za/co/absa/ui/AddUserForm.java
+++ Absa_WebApp_Assesment/src/test/java/za/co/absa/ui/AddUserForm.java
@@ -11,9 +11,9 @@ public class AddUserForm {
     public static Target PASSWORD_FIELD = Target.the("password field").locatedBy("//input[@name='Password']");
     public static Target AAA_RADIO_BUTTON = Target.the("AAA radio button").locatedBy("//label[1]//input[1]");
     public static Target BBB_RADIO_BUTTON = Target.the("BBB radio button").locatedBy("//label[2]//input[1]");
-    public static Target SALES_ROLE_OPTION = Target.the("sales option").locatedBy("//select[@name='RoleId']/option[1]");
-    public static Target CUSTOMER_ROLE_OPTION = Target.the("customer option").locatedBy("//select[@name='RoleId']/option[2]");
-    public static Target ADMIN_ROLE_OPTION = Target.the("admin option").locatedBy("//select[@name='RoleId']/option[3]");
+    public static Target SALES_ROLE_OPTION = Target.the("sales option").locatedBy("//option[contains(text(),'Sales Team')]");
+    public static Target CUSTOMER_ROLE_OPTION = Target.the("customer option").locatedBy("//option[contains(text(),'Customer')]");
+    public static Target ADMIN_ROLE_OPTION = Target.the("admin option").locatedBy("//option[contains(text(),'Admin')]");
     public static Target HEADING = Target.the("form heading").locatedBy("//h3[@class='ng-binding']");
     public static Target CLOSE_BUTTON = Target.the("close button").locatedBy("//button[@class='btn btn-danger']");
     public static Target SAVE_BUTTON = Target.the("save button").locatedBy("//button[@class='btn btn-success']");
